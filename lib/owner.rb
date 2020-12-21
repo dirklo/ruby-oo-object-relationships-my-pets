7a74be2cb12fec18f5eb1197d574b9dbec43b55d
Rick <dirklo@gmail.com>
2020-12-21 18:51:19 +0100

    commit tree c72331b2dd98d289c0825c4aacbf3500752229c0

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,67 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    self.class.all << self
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def cats
+    Cat.all.filter{|cat| cat.owner == self}
+  end
+
+  def dogs
+    Dog.all.filter{|dog| dog.owner == self}
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def walk_dogs
+    Dog.all.each{|dog| dog.mood = "happy"}
+  end
+
+  def feed_cats
+    Cat.all.each{|cat| cat.mood = "happy"}
+  end
+
+  def sell_pets
+    Cat.all.each do |cat| 
+      cat.mood = "nervous"
+      cat.owner = nil
+    end
+
+    Dog.all.each do |dog| 
+      dog.mood = "nervous"
+      dog.owner = nil
+    end
+  end
+
+  def list_pets
+    "I have #{dogs.count} dog(s), and #{cats.count} cat(s)."
+  end
 end
